d668f9d42bee8a12280b81e7470ed0054e771633
TaratipT <66070086@kmitl.ac.th>
2026-02-27 14:26:07 +0700

    fix navbar + profilepage

--- bidna/lib/app.dart
+++ bidna/lib/app.dart
@@ -1,31 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:bidna/screens/HomeScreen.dart';
 import 'package:bidna/screens/login_screen.dart';
 
-class App extends StatefulWidget {
+class App extends StatelessWidget {
   const App({super.key});
-
-  @override
-  State<App> createState() => _AppState();
-}
-
-class _AppState extends State<App> {
-  bool _isLoggedIn = false;
-  int _selectedIndex = 0;
-
-  // 2. รายชื่อหน้าต่างๆ (เรียงตามไอคอนข้างล่าง)
-  final List<Widget> _pages = [
-    const HomeScreen(),
-    const Center(child: Text("หน้า Sell")),
-    const Center(child: Text("หน้า Watchlist")),
-    const Center(child: Text("หน้า Profile")),
-  ];
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -37,34 +14,8 @@ class _AppState extends State<App> {
         primarySwatch: Colors.blue,
         scaffoldBackgroundColor: const Color.fromRGBO(238, 237, 237, 1),
       ),
-
-      home: _isLoggedIn
-          ? Scaffold(
-              body: _pages[_selectedIndex],
-              bottomNavigationBar: BottomNavigationBar(
-                currentIndex: _selectedIndex,
-                onTap: _onItemTapped,
-                items: const [
-                  BottomNavigationBarItem(
-                    icon: Icon(Icons.home),
-                    label: 'Home',
-                  ),
-                  BottomNavigationBarItem(
-                    icon: Icon(Icons.shopping_cart),
-                    label: 'Sell',
-                  ),
-                  BottomNavigationBarItem(
-                    icon: Icon(Icons.favorite),
-                    label: 'Watchlist',
-                  ),
-                  BottomNavigationBarItem(
-                    icon: Icon(Icons.person),
-                    label: 'Profile',
-                  ),
-                ],
-              ),
-            )
-          : const LoginScreen(),
+      // กำหนดให้หน้าแรกของแอปคือ LoginScreen
+      home: const LoginScreen(),
     );
   }
 }
